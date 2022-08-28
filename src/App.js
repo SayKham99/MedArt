c7de89ab0121c076293775483d489346a39174a1
SayKham99 <x5292621@gmail.com>
2022-08-28 21:37:07 +0500

    complete 6 sections

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import {Routes, Route} from "react-router-dom"
 import Navbar from "./components/Navbar/Navbar";
 import Nav from "./components/Navbar/Nav";
 import Home from "./components/Home/Home"
+import About from "./components/About/About";
 function App() {
   return (
     <div className="App">
@@ -9,6 +10,7 @@ function App() {
         <Nav />
       <Routes>
           <Route path="/" exact element={<Home />} />
+          <Route path="/about" exact element={<About />} />
       </Routes>
     </div>
   );
--- src/components/Navbar/Nav.jsx
+++ src/components/Navbar/Nav.jsx
@@ -1,42 +1,58 @@
-
 import {useEffect, useState} from "react";
 import logo from "../../assets/icons/logo.png";
 import MenuIcon from '@mui/icons-material/Menu';
 import CloseIcon from '@mui/icons-material/Close';
 import "./nav.scss"
-export default function Nav(){
+import {NavLink} from "react-router-dom";
+
+export default function Nav() {
 
     const [open, setOpen] = useState(false);
 
-    const changeMobile = function (){
+    const changeMobile = function () {
         setOpen(open === false ? true : false);
     }
-    return(
-        <section id="nav">
-           <div className="nav-wrapper">
-               <div className="logo">
-                <img src={logo} />
-               </div>
-               <div className={open ? "is-open" : "bars"}>
-                   <ul>
-                       <li>Home</li>
-                       <li>About Us</li>
-                       <li>Services</li>
-                       <li>Doctors</li>
-                       <li>News</li>
-                       <li>Blog</li>
-                       <li>Contact</li>
-                   </ul>
-               </div>
-               <div className="make-button">
-                   <button>Appointment</button>
-               </div>
-               <div className="hamburger" onClick={changeMobile}>
+    return (
+          <section id="nav">
+              <div className="nav-wrapper">
+                  <div className="logo">
+                      <img src={logo}/>
+                  </div>
+                  <div className={open ? "is-open" : "bars"}>
+                      <ul>
+                          <NavLink to={'/'}>
+                              <li>Home</li>
+                          </NavLink>
+                          <NavLink to={'/about'}>
+                              <li>About Us</li>
+                          </NavLink>
+                          <NavLink to={'/'}>
+                              <li>Services</li>
+                          </NavLink>
+                          <NavLink to={'/'}>
+                              <li>Doctors
+                              </li>
+                          </NavLink>
+                          <NavLink to={'/'}>
+                              <li>News</li>
+                          </NavLink>
+                          <NavLink to={'/'}>
+                              <li>Blog</li>
+                          </NavLink>
+                          <NavLink to={'/'}>
+                              <li>Contact</li>
+                          </NavLink>
+                      </ul>
+                  </div>
+                  <div className="make-button">
+                      <button>Appointment</button>
+                  </div>
+                  <div className="hamburger" onClick={changeMobile}>
                    <span className="icon">
-                       {open ? <CloseIcon /> : <MenuIcon />}
+                       {open ? <CloseIcon/> : <MenuIcon/>}
                    </span>
-               </div>
-           </div>
-        </section>
+                  </div>
+              </div>
+          </section>
     )
 }
